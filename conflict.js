53d7458821168f5ff7eb78bdbcc236b1b3a38503
gulsahatalan <atalangulsah@gmail.com>
2020-12-02 09:37:06 +0100

    Birinci satir degisti ikinci satir eklendi

--- conflict.js
+++ conflict.js
@@ -1,1 +1,2 @@
-console.log ("merhaba");  // Bu aciklama main branchde eklendi. 
+console.log ("hicoders");  // Bu aciklama main branchde eklendi.Branch1de iken degistirildi 
+const merge = 12   //Branch gul de iken eklendi 
